aadfb2bffd6c06dc5cfba77d33e9da0331512ba1
Thinh Pham <61043275+thinhpham3103@users.noreply.github.com>
2025-01-17 09:59:57 -0600

    Refactor app.py and style.css for improved readability and consistency. Updated formatting in app.py for better code style and adjusted CSS rules for consistent spacing and alignment across various elements.

--- app.py
+++ app.py
@@ -11,9 +11,10 @@ model = AutoModelForCausalLM.from_pretrained("microsoft/DialoGPT-medium")
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def index():
-    return render_template('chat.html')
+    return render_template("chat.html")
 
 
 @app.route("/get", methods=["GET", "POST"])
@@ -28,17 +29,27 @@ def get_Chat_response(text):
     # Let's chat for 5 lines
     for step in range(5):
         # encode the new user input, add the eos_token and return a tensor in Pytorch
-        new_user_input_ids = tokenizer.encode(str(text) + tokenizer.eos_token, return_tensors='pt')
+        new_user_input_ids = tokenizer.encode(
+            str(text) + tokenizer.eos_token, return_tensors="pt"
+        )
 
         # append the new user input tokens to the chat history
-        bot_input_ids = torch.cat([chat_history_ids, new_user_input_ids], dim=-1) if step > 0 else new_user_input_ids
+        bot_input_ids = (
+            torch.cat([chat_history_ids, new_user_input_ids], dim=-1)
+            if step > 0
+            else new_user_input_ids
+        )
 
-        # generated a response while limiting the total chat history to 1000 tokens, 
-        chat_history_ids = model.generate(bot_input_ids, max_length=1000, pad_token_id=tokenizer.eos_token_id)
+        # generated a response while limiting the total chat history to 1000 tokens,
+        chat_history_ids = model.generate(
+            bot_input_ids, max_length=1000, pad_token_id=tokenizer.eos_token_id
+        )
 
         # pretty print last ouput tokens from bot
-        return tokenizer.decode(chat_history_ids[:, bot_input_ids.shape[-1]:][0], skip_special_tokens=True)
+        return tokenizer.decode(
+            chat_history_ids[:, bot_input_ids.shape[-1] :][0], skip_special_tokens=True
+        )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run()
